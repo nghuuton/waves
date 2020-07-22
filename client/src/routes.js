fbdaac17e6c57cad88935f4a31fedfe0fe911b75
Tôn <nghuuton@gmail.com>
2020-07-22 14:56:00 +0700

    Login form && util formAction

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Layout from "./HOC/layout";
+import RegisterLogin from "./components/Register_login";
 
 const Routes = () => {
     return (
         <Layout>
             <Switch>
                 <Route path="/" exact component={Home} />
+                <Route path="/register-login" exact component={RegisterLogin} />
             </Switch>
         </Layout>
     );
