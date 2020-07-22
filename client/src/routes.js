1a05240293853130a728ad4c79b21b8bde3bcef7
Tôn <nghuuton@gmail.com>
2020-07-22 19:12:38 +0700

    Register Component

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Layout from "./HOC/layout";
 import RegisterLogin from "./components/Register_login";
+import Register from "./components/Register_login/register";
 
 const Routes = () => {
     return (
@@ -10,6 +11,7 @@ const Routes = () => {
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/register-login" exact component={RegisterLogin} />
+                <Route path="/register" exact component={Register} />
             </Switch>
         </Layout>
     );
