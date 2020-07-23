d0079cab3dd2709d58f9a55499aad6472029d2eb
Tôn <nghuuton@gmail.com>
2020-07-23 19:58:47 +0700

    Get product by sell,arrival Redux

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -2,7 +2,14 @@ import React, { Component } from "react";
 import HomeSlider from "./home_slider";
 import HomePromotion from "./home_promotion";
 
-export default class Home extends Component {
+import { connect } from "react-redux";
+import { getProductsBySell, getProductsByArrival } from "../../actions/products_actions";
+class Home extends Component {
+    componentDidMount() {
+        this.props.dispatch(getProductsBySell());
+        this.props.dispatch(getProductsByArrival());
+    }
+
     render() {
         return (
             <div>
@@ -12,3 +19,11 @@ export default class Home extends Component {
         );
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        products: state.products,
+    };
+}
+
+export default connect(mapStateToProps)(Home);
