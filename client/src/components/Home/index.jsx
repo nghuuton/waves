5e4e346470bd15fd623c08ac2b42040259267741
Tôn <nghuuton@gmail.com>
2020-07-23 13:31:03 +0700

    Sliders

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import HomeSlider from "./home_slider";
 
 export default class Home extends Component {
     render() {
-        return <div>Home</div>;
+        return (
+            <div>
+                <HomeSlider />
+            </div>
+        );
     }
 }
