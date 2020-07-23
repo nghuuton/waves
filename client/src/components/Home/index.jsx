02eadd181ae9362c30bb6bddd3a89899781ea6c0
Tôn <nghuuton@gmail.com>
2020-07-23 14:03:03 +0700

    Home Promotion

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import HomeSlider from "./home_slider";
+import HomePromotion from "./home_promotion";
 
 export default class Home extends Component {
     render() {
         return (
             <div>
                 <HomeSlider />
+                <HomePromotion />
             </div>
         );
     }
