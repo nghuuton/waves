249df4b77f5a6b2f644242d455e0b3996c39a1d9
Tôn <nghuuton@gmail.com>
2020-07-31 21:35:11 +0700

    fix something

--- server/server.js
+++ server/server.js
@@ -575,7 +575,6 @@ app.post("/api/user/reset-password", async (req, res) => {
 // });
 
 if (process.env.NODE_ENV === "production") {
-    const path = require("path");
     app.get("/*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
     });
