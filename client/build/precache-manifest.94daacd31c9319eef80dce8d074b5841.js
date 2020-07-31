120787585fa5f403ee6b769e975aced341e1ec42
Tôn <nghuuton@gmail.com>
2020-07-31 22:17:41 +0700

    deploy

---
+++ client/build/precache-manifest.94daacd31c9319eef80dce8d074b5841.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68dda5cf3ed3013a55e4f5b55a7db155",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a10fd5b34ad24d4ba50",
+    "url": "/static/css/main.57acbf86.chunk.css"
+  },
+  {
+    "revision": "42c0dcacfd2f57028561",
+    "url": "/static/js/2.9b55abef.chunk.js"
+  },
+  {
+    "revision": "4a5181d3ebbdc19fd486671b3d66c13e",
+    "url": "/static/js/2.9b55abef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a10fd5b34ad24d4ba50",
+    "url": "/static/js/main.d8e47911.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
